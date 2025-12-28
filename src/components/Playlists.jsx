f840f258dfdab322bc4c13f4c3361ea1b1ebcc87
lightning4747 <vignesh112847@gmail.com>
2025-12-28 17:14:43 +0530

    Init the playlists component

---
+++ src/components/Playlists.jsx
@@ -0,0 +1,11 @@
+export const Playlists = () => {
+    return (
+        <div className="playlists">
+            <h2>
+                <div className="create-playlist"></div>
+                <h3>Create New Playlists</h3>
+                <div className="playlist-form"></div>
+            </h2>
+        </div>
+    )
+}
