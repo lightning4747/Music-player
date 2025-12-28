214619300f6bae3658262985eca2c6f64d010883
lightning4747 <vignesh112847@gmail.com>
2025-12-28 17:11:37 +0530

    Config navbar and set up the routes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,14 @@
+import { Link, useLocation } from "react-router" 
+
 export const Navbar = () => {
-    return <nav className="navbar"></nav>
+    const location = useLocation();
+    return <nav className="navbar">
+        <div className="navbar-brand">
+        <Link className="brand-link" to="/">Muisc Player</Link>
+        </div>
+        <div className="navbar-links">
+            <Link to="/" className={`nav-link ${location.pathname==="/" ? "active" : ""}`}>All songs</Link>
+              <Link to="/player" className={`nav-link ${location.pathname==="/player" ? "active" : ""} `}>Playlist</Link>
+        </div>
+    </nav>
 }
