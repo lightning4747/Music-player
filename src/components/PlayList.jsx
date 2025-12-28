3049ef72bf962107d32ff911539fe485f3d8f5b1
lightning4747 <vignesh112847@gmail.com>
2025-12-28 17:15:59 +0530

    Fixed the issue with the duplicate files

--- src/components/PlayList.jsx
+++ src/components/PlayList.jsx
@@ -1,3 +1,11 @@
-export const PlayList = () => {
-    return <div>playlists</div>
+export const Playlists = () => {
+    return (
+        <div className="playlists">
+            <h2>
+                <div className="create-playlist"></div>
+                <h3>Create New Playlists</h3>
+                <div className="playlist-form"></div>
+            </h2>
+        </div>
+    )
 }
